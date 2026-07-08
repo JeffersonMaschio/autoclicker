448cc7e35fc541591a516d4b9ae0a9384a63600b
Jefferson Maschio <jeffersonluizdmaschio@gmail.com>
2026-07-08 18:06:44 -0300

    add "Click Repeat" to GUI

--- gui/gui.py
+++ gui/gui.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 
 app = ctk.CTk() 
 
-largura, altura = 350, 450 
+largura, altura = 450, 550 
 
 largura_tela = app.winfo_screenwidth()
 altura_tela = app.winfo_screenheight()
@@ -14,10 +14,10 @@ app.geometry(f"{largura}x{altura}+{x}+{y}")
 app.minsize(largura, altura)
 app.resizable(False, False)
 
-frame = ctk.CTkFrame(app, width=largura, height=altura,
+frame = ctk.CTkFrame(app, width=450, height=550,
     fg_color="#ff50ff"
 )
-frame.grid(pady=(20, 20), padx=(20, 20))
+frame.pack(pady=(2, 2), padx=(2, 0))
 
 #------------------
 # Funcoes
@@ -26,33 +26,34 @@ frame.grid(pady=(20, 20), padx=(20, 20))
 
 #------------------
 # Widgets 
-
-
-frameTime = ctk.CTkFrame(frame, width=(largura/2))
-frameTime.grid()
-
-frame.grid(row=0, column=0, padx=10, pady=10)
+# 
+# 
+frameTime = ctk.CTkFrame(frame, width=300, height=400,
+    fg_color="#ffff10"
+)
+frameTime.grid(row=0, column=0)
+ 
 # Hour
 labelHour = ctk.CTkLabel(frameTime, text="Hours") 
 labelHour.grid(row=0, column=0, padx=20, pady=(0, 10), sticky="w")
 
-entryHour = ctk.CTkEntry(frameTime, placeholder_text="0")
+entryHour = ctk.CTkEntry(frameTime, placeholder_text="0", width=50)
 entryHour.grid(row=0, column=1)
-
-
-# Minutes
+ 
+ 
+ # Minutes
 labelMin = ctk.CTkLabel(frameTime, text="Minutes")
 labelMin.grid(row=1, column=0, padx=20, pady=(0, 10))
 
-entryMin = ctk.CTkEntry(frameTime, placeholder_text="0")
+entryMin = ctk.CTkEntry(frameTime, placeholder_text="0", width=50)
 entryMin.grid(row=1, column=1)
 
-
+ 
 # Seconds
 labelSec = ctk.CTkLabel(frameTime, text="Seconds")
 labelSec.grid(row=2, column=0, padx=20, pady=(0, 10))
 
-entrySec = ctk.CTkEntry(frameTime, placeholder_text="0")
+entrySec = ctk.CTkEntry(frameTime, placeholder_text="0", width=50)
 entrySec.grid(row=2, column=1)
 
 
@@ -60,14 +61,23 @@ entrySec.grid(row=2, column=1)
 labelMil = ctk.CTkLabel(frameTime, text="Milisec")
 labelMil.grid(row=3, column=0, padx=20, pady=(0, 10))
 
-entryMil = ctk.CTkEntry(frameTime, placeholder_text="0")
+entryMil = ctk.CTkEntry(frameTime, placeholder_text="0", width=50)
 entryMil.grid(row=3, column=1)
 
-#++++++++++++++++++++++++
+#-------------
 
-#
+frameRepeat = ctk.CTkFrame(frame)
+frameRepeat.grid(row=0, column=1)
+
+labelRepeat = ctk.CTkLabel(frameRepeat, text="Click Repeat")
+labelRepeat.grid(row=0, column=5)
+ 
+cbRepeat = ctk.CTkCheckBox(frameRepeat, text="Repeat")
+cbRepeat.grid(row=1, column=5, sticky="w")
 
 
+cbRepeatStop = ctk.CTkCheckBox(frameRepeat, text="Repear until stoped")
+cbRepeatStop.grid(row=2, column=5, sticky="w")
 
 
 
